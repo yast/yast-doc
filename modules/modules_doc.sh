d84bd5d5fcd70b97607786d8ecf1aa6ef65ceeb1
Lukáš Ocilka <locilka@suse.cz>
2008-06-18 15:24:07 +0000

    One-file-docu
    Still needs some tuning, of course... ;)
    
    
    svn path=/trunk/doc/; revision=48399

--- modules/modules_doc.sh
+++ modules/modules_doc.sh
@@ -9,6 +9,7 @@ rm -rf html
 /usr/bin/xsltproc --xinclude yast2_modules.xslt ycpdoc.xml > index.xml
 /bin/mkdir -p ./html/
 /usr/bin/xsltproc --xinclude --stringparam html.stylesheet ${STYLESHEET_CSS} customize-html.xsl index.xml
+/usr/bin/xsltproc --xinclude --stringparam html.stylesheet ${STYLESHEET_CSS} ../customize-html-onefile.xsl index.xml > modules-onefile.html
 /bin/mkdir -pv style
 /bin/cp -av ../webpage/default.css style/
 /bin/mkdir -pv images
